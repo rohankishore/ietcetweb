4efb653716e85857fb037603271ff6cabdeb0a45
Rohan Kishore <rohankishore746@outlook.com>
2026-03-13 04:28:28 +0530

    sine wave dividers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,15 @@ function Home() {
   return (
     <div className="home">
       <DroneHero />
+      <div className="signal-splitter" aria-hidden="true">
+        <div className="signal-splitter__line" />
+        <div className="signal-splitter__scope">
+          <svg className="signal-splitter__sine" viewBox="0 0 1200 40" preserveAspectRatio="none">
+            <path d="M0 20 C 30 10, 60 10, 90 20 S 150 30, 180 20 S 240 10, 270 20 S 330 30, 360 20 S 420 10, 450 20 S 510 30, 540 20 S 600 10, 630 20 S 690 30, 720 20 S 780 10, 810 20 S 870 30, 900 20 S 960 10, 990 20 S 1050 30, 1080 20 S 1140 10, 1170 20 S 1230 30, 1260 20" />
+          </svg>
+          <span className="signal-splitter__pulse" />
+        </div>
+      </div>
       <ScrollLensShowcase />
       <section id="about" className="power-hub">
         <div className="container">
